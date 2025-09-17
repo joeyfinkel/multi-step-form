f7e6afc0601586ce2d2b1170cd25ea8197dc55dd
Joey Finkel <61193959+joeyfinkel@users.noreply.github.com>
2025-09-17 15:40:55 -0400

    adds core package

---
+++ packages/core/vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite';
+import dts from 'vite-plugin-dts';
+import { resolve } from 'path';
+
+export default defineConfig({
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/index.ts'),
+      name: 'MultiStepFormCore',
+      fileName: 'multi-step-form-core',
+    },
+  },
+  plugins: [dts()],
+});
